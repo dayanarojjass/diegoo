2a8b6cdc0077f85357f291670a9f4190e1df05f3
dayanarojjass <128360334+dayanarojjass@users.noreply.github.com>
2023-03-27 09:18:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,7 @@ let time = 0
 let start = 0
 input.onLogoEvent(TouchButtonEvent.Released, function () {
     time = input.runningTime() - start
-    basic.showNumber(time - 1000)
-    basic.pause(100)
+    basic.showNumber(Math.idiv(time, 1000))
     basic.clearScreen()
     basic.showNumber(time % 1000)
 })
